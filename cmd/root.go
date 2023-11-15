efe3b938be6d19b6b5ee4e135bbc34b88626c8ef
semihbkgr <buyukgungorsemih@gmail.com>
2023-11-15 22:19:11 +0300

    init module and command

---
+++ cmd/root.go
@@ -0,0 +1,34 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "yn",
+	Short: "yn yaml navigator",
+	Long: `yaml navigator
+yn < yaml
+`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		fmt.Fprintf(cmd.OutOrStdout(), "yn")
+		return nil
+	},
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	//rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/semihbkgr/yn/cmd"
+
+func main() {
+	cmd.Execute()
+}
